68e17a53f6851c55007d1570e9bacbce5d1bae4a
Wilfredo Diaz <wdiazdev@gmail.com>
2023-08-13 23:22:40 -0400

    under development

--- src/components/cashoncash/CashOnCash.tsx
+++ src/components/cashoncash/CashOnCash.tsx
@@ -6,7 +6,12 @@ const CashOnCash: FC = () => {
       <h2 className="text-white text-lg text-center mb-4 sm:text-lg text-md">
         Cash on Cash
       </h2>
-      <div className="border-1 rounded-lg p-4 border-zinc-800"></div>
+      <div
+        className="border-1 rounded-lg p-4 border-zinc-800 text-center 
+      text-white text-md text-zinc-500"
+      >
+        Under Development!
+      </div>
     </div>
   )
 }
