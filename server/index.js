22e67fab06f0d1ad7c3f1a18c999bf081353f710
Wilfredo Diaz <wilfredo.diaz@arbitersports.com>
2023-12-29 17:55:43 -0500

    Test route created

--- server/index.js
+++ server/index.js
@@ -1,13 +1,14 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRouter from "./routes/userRoute.js";
 
 dotenv.config();
 
 mongoose
   .connect(process.env.MONGODB)
   .then(() => {
-    console.log("Connected to Mongo");
+    console.log("Connected to MongoDB");
   })
   .catch((err) => {
     console.log(err);
@@ -16,5 +17,7 @@ mongoose
 const app = express();
 
 app.listen(3000, () => {
-  console.log("Server is running on port 3000!");
+  console.log("Server is running on port 3000");
 });
+
+app.use("/server", userRouter);
