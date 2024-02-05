156d98815886f5aa9068ca38b6c4af20b82d2155
Wilfredo Diaz <wdiazdev@gmail.com>
2024-02-05 09:50:49 -0500

    server side path updated

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ import userRouter from "./routes/userRoute.js"
 import authRouter from "./routes/authRoute.js"
 import listingRouter from "./routes/listingRoute.js"
 import cookieParser from "cookie-parser"
+import path from "path"
 
 dotenv.config()
 
@@ -16,6 +17,8 @@ mongoose
   .catch((err) => {
     console.log(err)
   })
+
+const __dirname = path.resolve()
 
 const app = express()
 
@@ -32,6 +35,12 @@ app.use("/api/user", userRouter)
 app.use("/api/auth", authRouter)
 
 app.use("/api/listing", listingRouter)
+
+app.use(express.static(path.join(__dirname, "/client/dist")))
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"))
+})
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500
